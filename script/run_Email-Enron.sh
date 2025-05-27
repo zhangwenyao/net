bac822be6c6a3808c51c0c8e37794d7522ce7eb1
zhangwenyao <zhangwy5@mail.ustc.edu.cn>
2025-05-27 10:38:24 +0800

    shell stat

--- script/run_Email-Enron.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for  i in `seq 0 9`; do
-  sleep 1
-  nohup ./bin/net.exe --config config/config_Email-Enron_beta0.05_gamma1_$i.json >  logs/Email-Enron_beta0.05_gamma1_$i.log 2>&1 &
-done
--- script/run_Facebook.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for  i in `seq 0 19`; do
-  sleep 1
-  nohup ./bin/net.exe --config config/config_Facebook_beta0.05_gamma1_$i.json > logs/Facebook_beta0.05_gamma1_$i.log 2>&1 &
-done
